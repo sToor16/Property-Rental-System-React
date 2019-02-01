27c20560cebee232f941e2421eae18350d0683eb
Shashank Gaikaiwari <shashank.gaikaiwari@gmail.com>
2019-02-01 00:14:49 -0600

    Key Abstracted on Git

--- src/components/property/property-detail/propertyMap.js
+++ src/components/property/property-detail/propertyMap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {MapWithGeoMarker} from "../../map/map";
+import { GOOGLE_API_KEY } from "../../../../server/config/config"
 
 export class PropertyMap extends Component {
 
@@ -7,7 +8,7 @@ export class PropertyMap extends Component {
         const location = this.props.location;
         return(
             <MapWithGeoMarker
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCurqTpMKeMSOLS5vy5ktnrHPAQM66UiXM&v=3.exp&libraries=geometry,drawing,places"
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `300px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
